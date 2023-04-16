9a8d3c628f1d0457549b689eb337619c019a46ec
Albert Xu <albertxu@freelamp.com>
2023-04-16 16:14:01 +0800

    add wpad.sh exe

--- pull_zone.sh
+++ pull_zone.sh
@@ -12,3 +12,6 @@ curl -sS $PROXY $URL -o /tmp/$CONF.xz
 [ -r $CONF_DIR/$CONF ] && mv $CONF_DIR/$CONF $CONF_DIR/$CONF.bak
 [ -d $CONF_DIR ] && echo "Info: Decompressing ..." && xzcat /tmp/$CONF.xz >$CONF_DIR/$CONF
 [ -x /usr/sbin/unbound-control ] && /usr/sbin/unbound-control reload
+# touch a .wpad file in your env. to tell unbound add wpad record after zone reload
+WORK_DIR=$(dirname $0)
+[ -r $WORK_DIR/.wpad -a -x $WORK_DIR/wpad/wpad.sh ] && $WORK_DIR/wpad/wpad.sh
