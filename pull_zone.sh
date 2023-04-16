bf3ff75db2edf71d9c0d5a44d8fa818de88f06b5
Albert Xu <albertxu@freelamp.com>
2023-04-16 16:17:55 +0800

    add echo lines

--- pull_zone.sh
+++ pull_zone.sh
@@ -7,10 +7,12 @@ PROXY="--proxy socks5h://wpad.local:2023"
 URL="https://raw.githubusercontent.com/LeisureLinux/adhole/main/data/adhole.conf.xz"
 CONF_DIR="/etc/unbound/adhole"
 CONF=$(basename $URL .xz)
+echo "Info: downloading zone config file from github ..."
 curl -sS $PROXY $URL -o /tmp/$CONF.xz
 [ $? != 0 ] && echo "Error: Download $URL failed!" && exit 1
 [ -r $CONF_DIR/$CONF ] && mv $CONF_DIR/$CONF $CONF_DIR/$CONF.bak
 [ -d $CONF_DIR ] && echo "Info: Decompressing ..." && xzcat /tmp/$CONF.xz >$CONF_DIR/$CONF
+rm /tmp/$CONF.xz
 [ -x /usr/sbin/unbound-control ] && /usr/sbin/unbound-control reload
 # touch a .wpad file in your env. to tell unbound add wpad record after zone reload
 WORK_DIR=$(dirname $0)
