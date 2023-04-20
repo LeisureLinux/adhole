74ca26514e95e0fc3893cd7ddd9b672991491901
Albert Xu <albertxu@freelamp.com>
2023-04-20 22:53:43 +0800

    change the logic to detect zone reload or not add add_wpad

--- pull_zone.sh
+++ pull_zone.sh
@@ -17,14 +17,23 @@ add_wpad() {
 	# write wpad.conf
 	HIP=$(hostname -I | awk '{print $1}')
 	[ "$(dig -4 +short wpad. @localhost)" = "$HIP" ] && echo "Info: No need to \
-        update wpad. record" && return
+update wpad. record" && return
 	echo "Adding wpad.local. record ..."
-	sudo unbound-control local_data wpad. A $HIP
-	sudo unbound-control local_data wpad.local. A $HIP
-	[ $? != 0 ] && echo "Error: not able to update wpad. record!" && exit 6
-	[ "$(dig -4 +short wpad.local. @localhost)" != "$HIP" ] && echo "Error: added wpad not able to resolv!" && exit 7
-	echo "Congrats, All is well! Added wpad. as $HIP"
+	# sudo unbound-control local_data wpad. A $HIP
+	# sudo unbound-control local_data wpad.local. A $HIP
+	cat >/etc/unbound/adhole/wpad.conf <<EOW
+local-zone: "wpad." transparent
+local-zone: "wpad.local." transparent
+local-data: "wpad. IN A $HIP"
+local-data: "wpad.local. IN A $HIP"
+EOW
+	RELOAD=1
+	# [ $? != 0 ] && echo "Error: not able to update wpad. record!" && exit 6
+	# [ "$(dig -4 +short wpad.local. @localhost)" != "$HIP" ] && echo "Error: added wpad not able to resolv!" && exit 7
+	# echo "Congrats, All is well! Added wpad. as $HIP"
 }
+
+add_wpad
 
 if [ ! -r $CONF_DIR/$CONF -o "$(find $CONF_DIR/$CONF -mtime +0 2>/dev/null)" ]; then
 	echo "Info: downloading zone config $CONF.zst file from github ..."
@@ -32,9 +41,9 @@ if [ ! -r $CONF_DIR/$CONF -o "$(find $CONF_DIR/$CONF -mtime +0 2>/dev/null)" ]; 
 	[ $? != 0 ] && echo "Error: Download $URL failed!" && exit 1
 	echo "Info: Decompressing ..." && zst -ck \
 		-d /tmp/$CONF.zst >$CONF_DIR/$CONF && rm /tmp/$CONF.zst
-	[ -x /usr/sbin/unbound-control ] && echo "Info: reloading unbound ..." && /usr/sbin/unbound-control reload
+	RELOAD=1
 else
 	echo "Info: $CONF is not expired yet."
 fi
 # [ -r $CONF_DIR/$CONF ] && mv $CONF_DIR/$CONF $CONF_DIR/$CONF.bak
-add_wpad
+[ "$RELOAD" = "1" -a -x /usr/sbin/unbound-control ] && echo "Info: reloading unbound ..." && /usr/sbin/unbound-control reload
