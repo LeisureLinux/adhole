e825054a011216555d1b1102e39bde989089c798
Albert Xu <albertxu@freelamp.com>
2023-04-22 11:30:42 +0800

    add status file support

--- data/adhole.sh
+++ data/adhole.sh
@@ -7,27 +7,31 @@
 # use $0 -s to skip the big text urls
 # touch ../.proxy as like: socks5://IP:port if need proxy
 CURL_TIME="--connect-timeout 15"
+SORT='parsort'
+[ ! -x /usr/bin/parsort ] && echo "Warning: missing package: parallel" && SORT='sort'
+[ ! -x /usr/bin/zst ] && echo "Error: to save space, please install zst package" && exit 1
 
 WORK_DIR=$(dirname $0)
 PFILE="$WORK_DIR/../.proxy"
 [ -r "$PFILE" ] && PROXY="--proxy $(cat $PFILE)"
-#
-CACHE_DIR=$(dirname $0)/.cache
+# default cache dir to use ~/.cache/adhole
+CACHE_DIR=$WORK_DIR/.cache
+[ -d $HOME/.cache ] && CACHE_DIR=$HOME/.cache/adhole
 [ ! -d $CACHE_DIR ] && mkdir -p $CACHE_DIR
-ZONE_FILE=$(dirname $0)/adhole.conf
-BLOCK_URL=$(dirname $0)/block_urls.txt
-# the contents of the URL in the list are only domain names plaintext
-TEXT_URL=$(dirname $0)/text_urls.txt
 #
-BLOCK_DOM=$(dirname $0)/block_domains.txt
-UNBLOCK_DOM=$(dirname $0)/unblock_domains.txt
+ZONE_FILE=$WORK_DIR/adhole.conf
+BLOCK_URL=$WORK_DIR/block_urls.txt
+# the contents of the URL in the list are only domain names plaintext
+TEXT_URL=$WORK_DIR/text_urls.txt
+BLOCK_DOM=$WORK_DIR/block_domains.txt
+UNBLOCK_DOM=$WORK_DIR/unblock_domains.txt
+#
 ZONE_TMP_FILE=/tmp/$(basename $ZONE_FILE).tmp
 cat /dev/null >$ZONE_TMP_FILE
 #
 STATUS="$WORK_DIR/status"
 
 touch $ZONE_FILE.zst $BLOCK_URL $BLOCK_DOM $UNBLOCK_DOM $TMP_FILE $TEXT_URL
-[ ! -x /usr/bin/zst ] && echo "Error: to save space, please install zst package" && exit 1
 
 counts() {
 	[ -r "$1" ] && echo "Info: Blocked $(grep "^local-zone" $1 | wc -l) domains"
@@ -48,21 +52,25 @@ grab_0000_head() {
 block_text() {
 	AD_URL=$1
 	local fname=$(basename $AD_URL)
-	[ "$fname" = "hosts" ] && fname=$(echo "$AD_URL" | awk -F/ '{print $(NF - 1)}')".hosts"
+	# for those with question mark, only grab the first part as filename
+	fname=$(echo $fname | awk -F'?' '{print $1}')
+	[ "$fname" = "hosts" ] && fname=$(echo "$AD_URL" | awk -F/ '{print $(NF - 1)}' | awk -F'?' '{print $1}')".hosts.txt"
 	TMP_FILE=$CACHE_DIR/$fname
 	file_age $TMP_FILE.status
 	if [ $? = 0 ]; then
 		echo "Info: no need to grab $AD_URL"
-		cat $TMP_FILE >>$ZONE_TMP_FILE
+		[ -r "$TMP_FILE" ] && cat $TMP_FILE >>$ZONE_TMP_FILE
 		return
 	fi
-	echo "Info: Grabbing $AD_URL ..."
-	# remove IPs in the list with grep -vP
+	echo "Info: Grabbing $AD_URL to $TMP_FILE ..."
 	curl $PROXY $CURL_TIME -sSL $AD_URL >$TMP_FILE.curl
 	[ $? != 0 ] && echo "Error: grab $AD_URL failed!" && return
 	echo "URL: $AD_URL" >$TMP_FILE.status
-	# Todo: grab head
-	head -15 $TMP_FILE.curl >>$TMP_FILE.status
+	head -30 $TMP_FILE.curl | grep '^#' | grep -v "#$" | grep . >$TMP_FILE.head
+	[ -s $TMP_FILE.head ] && cat $TMP_FILE.head >>$TMP_FILE.status || echo "# No head from source" >>$TMP_FILE.status
+	rm $TMP_FILE.head
+
+	# remove IPs in the list with grep -vP
 	grep -v "^#" $TMP_FILE.curl | dos2unix -k \
 		-q | sed 's/^0\.0\.0\.0 //g' | sed 's/^127\.0\.0\.1 //g' | grep \
 		. | grep -vP '\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b' | grep -v \
@@ -81,13 +89,13 @@ block() {
 	file_age $TMP_FILE.status
 	if [ $? = 0 ]; then
 		echo "Info: no need to grab $AD_URL"
-		cat $TMP_FILE >>$ZONE_TMP_FILE
+		[ -r "$TMP_FILE" ] && cat $TMP_FILE >>$ZONE_TMP_FILE
 		return
 	fi
-	echo "Info: Grabbing $AD_URL ..."
+	echo "Info: Grabbing $AD_URL to $TMP_FILE ..."
 	curl $PROXY $CURL_TIME -sSL $AD_URL >$TMP_FILE
 	[ $? != 0 ] && echo "Error: grab $AD_URL failed!" && return
-	# Pre-process remove some IP address
+	# Pre-process, remove some IP address
 	grep -E -v '127.0.0.1|255.255.255|::' $TMP_FILE >$TMP_FILE.curl
 	echo "URL: $AD_URL" >$TMP_FILE.status
 	grab_0000_head $TMP_FILE.curl >>$TMP_FILE.status
@@ -100,21 +108,30 @@ block() {
 grab_oisd() {
 	# Todo: Add back to Main
 	AD_URL="https://unbound.oisd.nl/"
+	local fname=$(basename $AD_URL)
+	[ "$fname" = "hosts" ] && fname=$(echo "$AD_URL" | awk -F/ '{print $(NF - 1)}')".hosts"
 	TMP_FILE=$CACHE_DIR/$(basename $AD_URL)
-	echo "Info: Grabbing $AD_URL ..."
-	curl $PROXY $CURL_TIME -sSL $AD_URL -o $TMP_FILE
-	if [ $? != 0 ]; then
-		echo "Error: grab $AD_URL failed!"
-	else
-		echo $AD_URL >$TMP_FILE.status
-		grep "^# Last modified:" >>$TMP_FILE.status
-		# head -10 $TMP_FILE > $(get
-		# awk '/Last modified:/ {print $NF}' $TMP_FILE
-		# : 2023-04-21T12:06:19+0000"
-		counts $TMP_FILE | tee -a $TMP_FILE.status
-		echo >$TMP_FILE.status
-		mv $TMP_FILE.status $TMP_FILE
+	file_age $TMP_FILE.status
+	if [ $? = 0 ]; then
+		echo "Info: no need to grab $AD_URL"
+		[ -r "$TMP_FILE" ] && cat $TMP_FILE >>$ZONE_TMP_FILE
+		return
 	fi
+	echo "Info: Grabbing $AD_URL to $TMP_FILE ..."
+	curl $PROXY $CURL_TIME -sSL $AD_URL -o $TMP_FILE
+	[ $? != 0 ] && echo "Error: grab $AD_URL failed!" && return
+	echo "URL: $AD_URL" >$TMP_FILE.status
+	grep "^#" $TMP_FILE | grep . >>$TMP_FILE.status
+	counts $TMP_FILE | tee -a $TMP_FILE.status
+	cat $TMP_FILE >>$ZONE_TMP_FILE
+}
+
+gen_status() {
+	cat /dev/null >$STATUS
+	for s in $(ls -1 $CACHE_DIR/*.status); do
+		grep . "$s" >>$STATUS
+		echo >>$STATUS
+	done
 }
 #
 # Main Prog.
@@ -123,6 +140,8 @@ if [ -n "$PROXY" ]; then
 	curl $PROXY $CURL_TIME -kIsS https://www.google.com/ >/dev/null
 	[ $? != 0 ] && echo "Error: Failed to check Google!" && exit 1
 fi
+
+grab_oisd
 
 for url in $(grep -v "^#" $BLOCK_URL); do
 	block $url
@@ -135,7 +154,7 @@ if [ "$1" != "-s" ]; then
 	done
 fi
 
-#
+# Todo: add status file
 echo "Info: Add local block domain list ..."
 grep -v "^#" $BLOCK_DOM | awk '{print "local-zone: \"" $1 "\" always_null"}' >>$ZONE_TMP_FILE
 # counts $TMP_FILE
@@ -153,9 +172,10 @@ EOH
 exclude_domain=$(grep -v "^#" $UNBLOCK_DOM | xargs | tr " " "|")
 # e.g. exclude_domain="as.weixin.qq.com|pandora.xiaomi.com|cm.bilibili.com"
 echo "Info: deduplicating ..."
-grep -v "0.0.0.0" $ZONE_TMP_FILE | sed -e 's/\."/"/g' | grep -E -v "$exclude_domain" | sort | uniq >$ZONE_FILE
+grep -v "0.0.0.0" $ZONE_TMP_FILE | sed -e 's/\."/"/g' | grep -E -v "$exclude_domain" | $SORT -u >$ZONE_FILE
 rm $ZONE_TMP_FILE
 echo "Info: results after deduplication:"
 counts $ZONE_FILE
 echo "Info: compressing $ZONE_FILE ..."
 zst $ZONE_FILE
+gen_status
