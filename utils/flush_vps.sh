b3c5960fb8c70cf94ebebeb0e5a18449880c68f2
Albert Xu <albertxu@freelamp.com>
2023-11-06 21:28:57 +0800

    remove specific domain in script, pass it as parameter

--- utils/flush_vps.sh
+++ utils/flush_vps.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # 检查本地解析和远端解析是否一致，如果不一致则清理本地 DNS 缓存
-DOMAIN="vps.freelamp.com."
-L_IP=$(dig -4 +short $DOMAIN @127.0.0.1)
-G_IP=$(dig -4 +short $DOMAIN @8.8.8.8)
-[ -z "$L_IP" ] && echo "Error in local DNS resolve!" && exit 1
-[ -z "$G_IP" ] && echo "Error in Google DNS resolve!" && exit 1
-[ "$L_IP" != "$G_IP" ] && echo "Local: $L_IP; Google: $G_IP" && unbound-control flush $DOMAIN
+[ ! -x /usr/sbin/unbound-control ] && echo "Error: need to be able to exec: unbound-control" && exit 1
+for d in $@; do
+	L_IP=$(dig -4 +short $d @127.0.0.1)
+	G_IP=$(dig -4 +short $d @8.8.8.8)
+	[ -z "$L_IP" ] && echo "Error in local DNS resolve!" && exit 10
+	[ -z "$G_IP" ] && echo "Error in Google DNS resolve!" && exit 11
+	[ "$L_IP" != "$G_IP" ] && echo "$d: Local: $L_IP; Google: $G_IP" && unbound-control flush $d
+done
