f62ba6a364f1bbd26c270ccd269e82b64c6ae992
Albert Xu <albertxu@freelamp.com>
2023-12-16 14:46:17 +0800

    add unbound-checkconf to verify the result, add tr command to conver upper-case zone name to lower-case, add gh-upload.sh

--- data/adhole.sh
+++ data/adhole.sh
@@ -8,71 +8,71 @@
 # touch ../.proxy as like: socks5://IP:port if need proxy
 CURL_TIME="--connect-timeout 15"
 SORT='parsort'
+PROXY=""
 [ ! -x /usr/bin/parsort ] && echo "Warning: missing package: parallel" && SORT='sort'
 [ ! -x /usr/bin/zst ] && echo "Error: to save space, please install zst package" && exit 1
 
-WORK_DIR=$(dirname $0)
-[ ! -d $WORK_DIR/result ] && mkdir $WORK_DIR/result
-PFILE="$WORK_DIR/../.proxy"
-[ -s "$PFILE" ] && PROXY="--proxy $(cat $PFILE)"
+WORK_DIR=$(dirname "$0")
+[ ! -d "$WORK_DIR"/result ] && mkdir "$WORK_DIR"/result
+[ -s "$HOME/.proxy" ] && PROXY="-x $(cat "$HOME/.proxy")"
+CURL="/usr/bin/curl ${PROXY} ${CURL_TIME} --compressed -sSL"
 
-CURL="curl $PROXY $CURL_TIME --compressed -sSL"
-
-# Default cache dir to use ~/.cache/adhole
-CACHE_DIR=$WORK_DIR/.cache
-[ -d $HOME/.cache ] && CACHE_DIR=$HOME/.cache/adhole
-[ ! -d $CACHE_DIR ] && mkdir -p $CACHE_DIR
+CACHE_DIR=$HOME/.cache/adhole
+[ ! -d "$CACHE_DIR" ] && mkdir -p "$CACHE_DIR"
 #
 ZONE_FILE=$WORK_DIR/result/adhole.conf
+STATUS="$WORK_DIR"/result/adhole_status.txt
+#
 BLOCK_URL=$WORK_DIR/block_urls.txt
 # the contents of the URL in the list are only domain names plaintext
 TEXT_URL=$WORK_DIR/text_urls.txt
 BLOCK_DOM=$WORK_DIR/block_domains.txt
 UNBLOCK_DOM=$WORK_DIR/unblock_domains.txt
-#
-ZONE_TMP_FILE=/tmp/$(basename $ZONE_FILE).tmp
-cat /dev/null >$ZONE_TMP_FILE
-#
-STATUS="$WORK_DIR/result/adhole.status"
+ZONE_TMP_FILE=/tmp/$(basename "${ZONE_FILE}").tmp
 
-touch $ZONE_FILE.zst $BLOCK_URL $BLOCK_DOM $UNBLOCK_DOM $TMP_FILE $TEXT_URL
-
+cat /dev/null >"$ZONE_TMP_FILE"
+touch "$ZONE_FILE".zst "$BLOCK_URL" "$BLOCK_DOM" "$UNBLOCK_DOM" "$TEXT_URL"
+#
 counts() {
-	[ -r "$1" ] && echo "Info: Blocked $(grep "^local-zone" $1 | wc -l) domains"
+	[ -r "$1" ] && echo "Info: Blocked $(grep -c "^local-zone" "$1") domains"
 }
 
 file_age() {
 	[ ! -r "$1" ] && return 2
-	local file_age=$(date -r $1 +"%s")
-	local now=$(date +"%s")
+	local file_age
+	file_age=$(date -r "$1" +"%s")
+	local now
+	now=$(date +"%s")
 	[ $(($now - $file_age)) -gt 86400 ] && return 1 || return 0
 }
 
 grab_0000_head() {
 	[ ! -r "$1" ] && return
-	[ "$(grep -v '^#' $1 | grep . | tail -1 | awk '{print $1}')" = "0.0.0.0" ] && sed -n '1,/^0.0.0.0/p' $1 | grep -v "^0.0.0.0"
+	[ "$(grep -v '^#' "$1" | grep . | tail -1 | awk '{print $1}')" = "0.0.0.0" ] && sed -n '1,/^0.0.0.0/p' "$1" | grep -v "^0.0.0.0"
 }
 
 block_text() {
 	AD_URL=$1
-	local fname=$(basename $AD_URL)
+	local fname
+	fname=$(basename "$AD_URL")
 	# for those with question mark, only grab the first part as filename
-	fname=$(echo $fname | awk -F'?' '{print $1}')
+	fname=$(echo "$fname" | awk -F'?' '{print $1}')
 	[ "$fname" = "hosts" ] && fname=$(echo "$AD_URL" | awk -F/ '{print $(NF - 1)}' | awk -F'?' '{print $1}')".hosts.txt"
 	TMP_FILE=$CACHE_DIR/$fname
-	file_age $TMP_FILE.status
-	if [ $? = 0 ]; then
+	if file_age "$TMP_FILE".status; then
 		echo "Info: no need to grab $AD_URL"
-		[ -r "$TMP_FILE" ] && cat $TMP_FILE >>$ZONE_TMP_FILE
+		[ -r "$TMP_FILE" ] && cat "$TMP_FILE" >>"$ZONE_TMP_FILE"
 		return
 	fi
 	echo "Info: Grabbing $AD_URL to $TMP_FILE ..."
-	$CURL $AD_URL >$TMP_FILE.curl
-	[ $? != 0 ] && echo "Error: grab $AD_URL failed!" && return
-	echo "URL: $AD_URL" >$TMP_FILE.status
-	head -30 $TMP_FILE.curl | grep '^#' | grep -v "#$" | grep . >$TMP_FILE.head
-	[ -s $TMP_FILE.head ] && cat $TMP_FILE.head >>$TMP_FILE.status || echo "# No head from source" >>$TMP_FILE.status
-	rm $TMP_FILE.head
+	if ! $CURL "$AD_URL" >"$TMP_FILE".curl; then
+		echo "Error: grab $AD_URL failed!"
+		return
+	fi
+	echo "URL: $AD_URL" >"$TMP_FILE".status
+	head -30 "$TMP_FILE".curl | grep '^#' | grep -v "#$" | grep . >"$TMP_FILE".head
+	[ -s "$TMP_FILE".head ] && cat "$TMP_FILE".head >>"$TMP_FILE".status || echo "# No head from source" >>"$TMP_FILE".status
+	rm "$TMP_FILE".head
 
 	# remove IPs in the list with grep -vP
 	grep -v "^#" $TMP_FILE.curl | dos2unix -k \
@@ -87,7 +87,8 @@ block_text() {
 block() {
 	# convert to unbound always_null syntax
 	AD_URL=$1
-	local fname=$(basename $AD_URL)
+	local fname
+	fname=$(basename "$AD_URL")
 	[ "$fname" = "hosts" ] && fname=$(echo "$AD_URL" | awk -F/ '{print $(NF - 1)}')".hosts"
 	TMP_FILE=$CACHE_DIR/$fname
 	file_age $TMP_FILE.status
@@ -113,76 +114,92 @@ block() {
 grab_oisd() {
 	AD_URL="https://unbound.oisd.nl/"
 	# curl -sS -L --compressed "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound&showintro=0&mimetype=plaintext"
-	local fname=$(basename $AD_URL)
+	local fname
+	fname=$(basename "$AD_URL")
 	[ "$fname" = "hosts" ] && fname=$(echo "$AD_URL" | awk -F/ '{print $(NF - 1)}')".hosts"
 	TMP_FILE=$CACHE_DIR/$(basename $AD_URL)
-	file_age $TMP_FILE.status
-	if [ $? = 0 ]; then
+	if file_age "$TMP_FILE".status; then
 		echo "Info: no need to grab $AD_URL"
-		[ -r "$TMP_FILE" ] && cat $TMP_FILE >>$ZONE_TMP_FILE
+		[ -r "$TMP_FILE" ] && cat "$TMP_FILE" >>"$ZONE_TMP_FILE"
 		return
 	fi
 	echo "Info: Grabbing $AD_URL to $TMP_FILE ..."
-	$CURL $AD_URL -o $TMP_FILE
-	[ $? != 0 ] && echo "Error: grab $AD_URL failed!" && return
-	echo "URL: $AD_URL" >$TMP_FILE.status
-	grep "^#" $TMP_FILE | grep . >>$TMP_FILE.status
-	counts $TMP_FILE | tee -a $TMP_FILE.status
-	cat $TMP_FILE >>$ZONE_TMP_FILE
+	if ! $CURL $AD_URL -o "$TMP_FILE"; then
+		echo "Error: grab $AD_URL failed!" && return
+	fi
+	echo "URL: $AD_URL" >"$TMP_FILE".status
+	grep "^#" "$TMP_FILE" | grep . >>"$TMP_FILE".status
+	counts "$TMP_FILE" | tee -a "$TMP_FILE".status
+	cat "$TMP_FILE" >>"$ZONE_TMP_FILE"
 }
 
 gen_status() {
-	echo "# =========================================" >$STATUS
-	echo "# LeisureLinux Adhole block domains sources " >>$STATUS
-	echo "# UpdateTime: $(date +"%Y-%m-%dT%H:%M:%S%z") " >>$STATUS
-	echo "# =========================================" >>$STATUS
-	echo "#" >>$STATUS
-	for s in $(ls -1 $CACHE_DIR/*.status); do
-		grep . "$s" >>$STATUS
-		echo >>$STATUS
+	echo "# =========================================" >"$STATUS"
+	(
+		echo "# LeisureLinux Adhole block domains sources "
+		echo "# UpdateTime: $(date +"%Y-%m-%dT%H:%M:%S%z") "
+		echo "# ========================================="
+		echo "#"
+	) >>"$STATUS"
+	for s in "$CACHE_DIR"/*.status; do
+		grep . "$s" >>"$STATUS"
+		echo >>"$STATUS"
 	done
 }
 #
 # Main Prog.
 if [ -n "$PROXY" ]; then
-	echo "Info: Using: $PROXY checking Google ..."
-	curl $PROXY $CURL_TIME -kIsS https://www.google.com/ >/dev/null
-	[ $? != 0 ] && echo "Error: Failed to check Google!" && exit 1
+	echo "Info: Checking proxy healthy..."
+	if ! curl ${PROXY} ${CURL_TIME} -kIsS https://www.google.com/ >/dev/null; then
+		echo "Error: Failed to check Google!"
+		exit 1
+	else
+		echo "Info: Proxy server reached google.com OK."
+	fi
 fi
 
 grab_oisd
 
-for url in $(grep -v "^#" $BLOCK_URL); do
-	block $url
+for url in $(grep -v "^#" "$BLOCK_URL"); do
+	block "$url"
 done
 
 # use $0 -s to skip the big text urls to avoid huge zone file on small SBC
 if [ "$1" != "-s" ]; then
-	for url in $(grep -v "^#" $TEXT_URL); do
-		block_text $url
+	for url in $(grep -v "^#" "$TEXT_URL"); do
+		block_text "$url"
 	done
 fi
 
 echo "Info: Add local block domain list ..."
-grep -v "^#" $BLOCK_DOM | grep . | awk '{print "local-zone: \"" $1 "\" always_null"}' >>$ZONE_TMP_FILE
+grep -v "^#" "$BLOCK_DOM" | grep . | awk '{print "local-zone: \"" $1 "\" always_null"}' >>"$ZONE_TMP_FILE"
 #
-mv $ZONE_FILE.zst $ZONE_FILE.zst.old 2>/dev/null
+mv "$ZONE_FILE".zst "$ZONE_FILE".zst.old 2>/dev/null
 # Add head
 T=$(date +"%Y-%m-%dT%H:%M:%S%z")
-cat >$ZONE_FILE <<EOH
+cat >"$ZONE_FILE" <<EOH
 # Syntax: unbound
 # Source: LeisureLinux
 # URL: https://github.com/LeisureLinux/adhole
 # UpdateTime: $T
 EOH
 # remove unblock domains from the generated block list and deduplicate
-exclude_domain=$(grep -v "^#" $UNBLOCK_DOM | xargs | tr " " "|")
+exclude_domain=$(grep -v "^#" "$UNBLOCK_DOM" | xargs | tr " " "|")
 # e.g. exclude_domain="as.weixin.qq.com|pandora.xiaomi.com|cm.bilibili.com"
 echo "Info: deduplicating ..."
-time grep -v "0.0.0.0" $ZONE_TMP_FILE | sed -e 's/\."/"/g' | grep -E -v "$exclude_domain" | $SORT -u >>$ZONE_FILE
-rm $ZONE_TMP_FILE
+time grep -v "0.0.0.0" "$ZONE_TMP_FILE" | sed -e 's/\."/"/g' | grep -E -v "$exclude_domain" | tr '[A-Z]' '[a-z]' | $SORT -u >>"$ZONE_FILE"
+rm "$ZONE_TMP_FILE"
 echo "Info: results after deduplication:"
-counts $ZONE_FILE
-echo "Info: compressing $ZONE_FILE ..."
-zst $ZONE_FILE
-gen_status
+counts "$ZONE_FILE"
+cat >/tmp/check.conf <<EOF
+server:
+include: "$ZONE_FILE"
+EOF
+if ! /usr/sbin/unbound-checkconf /tmp/check.conf; then
+	echo "Error: Found error in $ZONE_FILE"
+else
+	echo "Info: compressing $ZONE_FILE ..."
+	zst "$ZONE_FILE"
+	gen_status
+	"${WORK_DIR}"/gh-upload.sh
+fi
