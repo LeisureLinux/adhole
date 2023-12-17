eeb904b667104a4309a5451b50977fad8dd6970d
Albert Xu <albertxu@freelamp.com>
2023-12-17 12:41:12 +0800

    删除 zone 文件的屏幕打印

--- pull_zone.sh
+++ pull_zone.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 # Run as root cron
-# pull the latest adhole.conf.zst from github
+# pull the latest adhole.conf.zst from github, zone file already verified before uploading to github.
 # put your proxy server e.g. http://IP:port or socks5://IP:port to ~/.proxy
-WORK_DIR=$(dirname $0)
 PFILE="$HOME/.proxy"
-[ -r "$PFILE" ] && PROXY="--proxy $(cat $PFILE)" || echo "设置代理服务器能加速从 github 拉取 zone 文件的速度"
+[ -r "$PFILE" ] && PROXY="--proxy $(cat "$PFILE")" || echo "设置代理服务器能加速从 github 拉取 zone 文件的速度"
 [ ! -x /usr/bin/zstd ] && echo "Error: Please install zstd package" && exit
 #
 URL="https://github.com/LeisureLinux/adhole/releases/download/adhole/adhole.conf.zst"
-
+# Only check local zone file 7 days old
+DAYS=7
+#
 CONF="adhole.conf"
 STATUS="adhole.status"
 CONF_DIR="/etc/unbound/adhole"
@@ -21,7 +22,7 @@ if [ -r "$1" ]; then
 	sudo zstd -f -d /etc/unbound/adhole/"$(basename "$1")"
 	RELOAD=1
 else
-	if [ ! -r $CONF_DIR/$CONF ] || [ "$(find $CONF_DIR/$CONF -mtime +1 2>/dev/null)" ]; then
+	if [ ! -r $CONF_DIR/$CONF ] || [ "$(find $CONF_DIR/$CONF -mtime +"$DAYS" 2>/dev/null)" ]; then
 		echo "Info: Downloading zone config $CONF.zst file from github ..."
 		if ! curl -SL "$PROXY" "$URL" -o "/tmp/$CONF.zst"; then
 			echo "Error: Download $URL failed!"
@@ -29,13 +30,13 @@ else
 		fi
 		# grab status
 		curl -sSL "$PROXY" "$(dirname $URL)/$STATUS" -o "/tmp/$STATUS"
-		grep -v ^# /tmp/$STATUS | grep .
+		# grep -v ^# /tmp/$STATUS | grep .
 		head -4 /tmp/$STATUS
 		echo "Info: Decompressing ..." && zstd -ck \
 			-d /tmp/$CONF.zst | sudo tee $CONF_DIR/$CONF && rm /tmp/$CONF.zst
 		RELOAD=1
 	else
-		echo "Info: $CONF is not expired yet."
+		echo "Info: $CONF is not expired yet, need $DAYS days to expire to avoid reload zone too frequently."
 	fi
 fi
 [ "$RELOAD" = "1" ] && [ -x /usr/sbin/unbound-control ] && echo "Info: reloading unbound ..." && sudo /usr/sbin/unbound-control reload
