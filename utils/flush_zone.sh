b792fbdff2823cbbc4c37c74d6eea9d0fd567445
Albert Xu <albertxu@freelamp.com>
2024-04-24 21:05:10 +0800

    add more condition check

--- utils/flush_zone.sh
+++ utils/flush_zone.sh
@@ -4,11 +4,16 @@
 [ -z "$1" ] && echo "Syntax: $0 hostname" && exit 2
 [ "$UID" != 0 ] && echo "Error: Need root to run!" && exit 3
 for d in "$@"; do
-    L_IP=$(dig -tA +short "$d" @127.0.0.1 2>/dev/null)
-    [ -z "$L_IP" ] && echo "Error in local resolving $d!" && exit 10
-    G_IP=$(dig -tA +short "$d" @8.8.8.8 2>/dev/null)
-    [ -z "$G_IP" ] && echo "Error in Google resolving $d!" && exit 11
-    [ "$L_IP" != "$G_IP" ] && echo "\"$d\": Local: $L_IP; Google: $G_IP" && /usr/sbin/unbound-control flush "$d"
-    echo "Sleeping 10 seconds ..."
-    sleep 10
+	L_IP=$(dig -tA +short "$d" @127.0.0.1 2>/dev/null)
+	[ -z "$L_IP" ] && echo "Error in local resolving $d!" && exit 10
+	G_IP=$(dig -tA +short "$d" @8.8.8.8 2>/dev/null)
+	[ -z "$G_IP" ] && echo "Error in Google resolving $d!" && exit 11
+	if [ "$L_IP" != "$G_IP" ]; then
+		echo "\"$d\": Local: $L_IP; Google: $G_IP, flushing zone ..."
+		/usr/sbin/unbound-control flush "$d"
+		echo "Sleeping 10 seconds ..."
+		sleep 10
+	else
+		echo "Domain $d  is good."
+	fi
 done
