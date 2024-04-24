06e6c3a7e2b71e3b19353fae9fe893226e3d2f0c
Albert Xu <albertxu@freelamp.com>
2024-04-24 20:48:42 +0800

    add root user detection

--- utils/flush_zone.sh
+++ utils/flush_zone.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 # 检查本地解析和远端解析是否一致，如果不一致则清理本地 DNS 缓存
 [ ! -x /usr/sbin/unbound-control ] && echo "Error: need to be able to exec: unbound-control" && exit 1
+[ -z "$1" ] && echo "Syntax: $0 hostname" && exit 2
+[ "$UID" != 0 ] && echo "Error: Need root to run!" && exit 3
 for d in "$@"; do
-	L_IP=$(dig -tA +short "$d" @127.0.0.1)
-	[ -z "$L_IP" ] && echo "Error in local resolving $d!" && exit 10
-	G_IP=$(dig -tA +short "$d" @8.8.8.8)
-	[ -z "$G_IP" ] && echo "Error in Google resolving $d!" && exit 11
-	[ "$L_IP" != "$G_IP" ] && echo "\"$d\": Local: $L_IP; Google: $G_IP" && unbound-control flush "$d"
-	echo "Sleeping 10 seconds ..."
-	sleep 10
+    L_IP=$(dig -tA +short "$d" @127.0.0.1 2>/dev/null)
+    [ -z "$L_IP" ] && echo "Error in local resolving $d!" && exit 10
+    G_IP=$(dig -tA +short "$d" @8.8.8.8 2>/dev/null)
+    [ -z "$G_IP" ] && echo "Error in Google resolving $d!" && exit 11
+    [ "$L_IP" != "$G_IP" ] && echo "\"$d\": Local: $L_IP; Google: $G_IP" && /usr/sbin/unbound-control flush "$d"
+    echo "Sleeping 10 seconds ..."
+    sleep 10
 done
